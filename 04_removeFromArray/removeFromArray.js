471094c5a405b4eadf29bc2bfae5826d04684abf
Edward Brecht <edbrecht@gmail.com>
2023-07-23 11:54:58 -0700

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, rem) {
+    for (let i = 1; i < arguments.length; i++)
+    arr = arr.filter(item => item !== arguments[i]);
+    return arr;
 };
 
 // Do not edit below this line
