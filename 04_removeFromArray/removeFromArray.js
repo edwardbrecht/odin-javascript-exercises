fdf84e9888cee5fe0ea3053e391d7e6c9a18fce0
Edward Brecht <edbrecht@gmail.com>
2023-07-23 11:58:13 -0700

    Update function with proper parameter names

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function(arr, rem) {
+const removeFromArray = function(arr, ...args) {
     for (let i = 1; i < arguments.length; i++)
     arr = arr.filter(item => item !== arguments[i]);
     return arr;
