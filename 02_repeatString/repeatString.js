4424d14dd9446855942aeedf3e5204c8041c21b5
Edward Brecht <edbrecht@gmail.com>
2023-07-23 11:32:08 -0700

    Solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {return "ERROR"};
+    let repStr = "";
+    for (let i = 0; i < num; i++) {
+        repStr = repStr.concat(str);
+    }
+    return repStr;
 };
 
 // Do not edit below this line
