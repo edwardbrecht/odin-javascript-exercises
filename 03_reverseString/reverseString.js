1728f0b042bff1e19bb0333aa56c11a8afe830f3
Edward Brecht <edbrecht@gmail.com>
2023-07-23 11:45:11 -0700

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revStr = ""
+    for (let i = 1; i <= str.length; i++)
+    {
+        revStr = revStr.concat(str.substr(-i, 1));
+    }
+    return revStr;
 };
 
 // Do not edit below this line
