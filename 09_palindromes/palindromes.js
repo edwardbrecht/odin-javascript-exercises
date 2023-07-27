0230c13820e30574c8783e957bf754ec1d9d6b10
Edward Brecht <edbrecht@gmail.com>
2023-07-27 16:54:07 -0700

    Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const splitStr = newStr.split("");
+    const reverseArr = splitStr.reverse();
+    const reverseStr = reverseArr.join("");
+    return newStr === reverseStr;
+    
 };
 
 // Do not edit below this line
